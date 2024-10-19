7beb5dc4fd653813abe900ca4d0c686407b99530
Gabriel Cánepa <gacanepa@gmail.com>
2024-10-18 23:23:12 -0300

    Add event listeners for inputs

---
+++ 00-elements/00-inputs/inputs.js
@@ -0,0 +1,19 @@
+import { MIN_LENGTH_THRESHOLD } from "./constants.js";
+
+const dataList = document.getElementById("nba-team-choice");
+dataList.addEventListener("change", function(event) {
+  console.log(event.target.value);
+});
+á
+const inputText = document.getElementById("input-text");
+const inputTextMaxLength = inputText.getAttribute("maxlength");
+const inputTextError = document.getElementById("input-text-error");
+inputText.addEventListener("input", function(event) {
+  const inputTextCurrentLength = event.target.value.length;
+  const charactersLeft = inputTextMaxLength - inputTextCurrentLength;
+  if (charactersLeft <= MIN_LENGTH_THRESHOLD) {
+    inputTextError.textContent = `You have ${charactersLeft} characters left`;
+  } else {
+    inputTextError.textContent = "";
+  }
+});
